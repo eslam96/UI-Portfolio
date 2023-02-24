c0c40ec627ddf6f87fe66ccc297a6e533fdd5a26
DESKTOP-V5MPCHA\Eslam <eslam19096@gmail.com>
2023-02-24 13:46:00 +0200

    creating sanity backend schema files

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import works from './works'
+import workExperience from './workExperience'
+import brands from './brands'
+
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  contact,
+  experiences,
+  works,
+  workExperience,
+  skills,
+  brands,
+]
